85d0c3121193dfce1158aed8fde36f51a011619d
yangy <1355285815@qq.com>
2024-06-14 09:45:31 +0800

    'global按钮消失China按钮出现'

--- Scripts/Global/CtrBtn.cs
+++ Scripts/Global/CtrBtn.cs
@@ -9,12 +9,12 @@ public class CtrBtn : MonoBehaviour
     public List<TextMeshProUGUI> textMeshProUGUIs;
 
     public List<Image> imageList;
-    
+
     public List<GameObject> Btns;
 
     public float duration = 2.0f;
 
-    private Color unSelectedColor = new(1f, 1f, 1f,1f);
+    private Color unSelectedColor = new(1f, 1f, 1f, 1f);
 
     private Color selectedColor = new(8 / 255f, 64 / 255f, 248 / 255f, 1f);
 
@@ -30,7 +30,7 @@ public class CtrBtn : MonoBehaviour
             }
             else
             {
-                StartCoroutine(ChangeColorOverTime(textMeshProUGUIs[index], imageList[index],  selectedColor,unSelectedColor, duration));
+                StartCoroutine(ChangeColorOverTime(textMeshProUGUIs[index], imageList[index], selectedColor, unSelectedColor, duration));
             }
         });
     }
@@ -59,11 +59,55 @@ public class CtrBtn : MonoBehaviour
             float icolorG = Mathf.SmoothStep(textStartColor.g, textColor.g, t);
             float icolorB = Mathf.SmoothStep(textStartColor.b, textColor.b, t);
             text.color = new(icolorR, icolorG, icolorB, 1f);
-         
+
             yield return null;
         }
         btn.color = btnColor;
         text.color = textColor;
+        yield return new WaitForSeconds(4f);
+        HideBtn();
+    }
+
+
+    public void HideBtn()
+    {
+        imageList.ForEach(i =>
+        {
+            StartCoroutine(ChangeBtnDisapper(i, duration));
+        });
+        textMeshProUGUIs.ForEach(t =>
+        {
+            StartCoroutine(ChangeTextDisapper(t, duration));
+        });
+    }
+    IEnumerator ChangeTextDisapper(TextMeshProUGUI text, float duration)
+    {
+        float elapsed = 0.0f;
+        Color textStartColor = text.color;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration;
+            float icolorA = Mathf.SmoothStep(textStartColor.a, 0, t);
+            text.color = new(textStartColor.r, textStartColor.g, textStartColor.b, icolorA);
+            yield return null;
+        }
+
+    }
+
+    IEnumerator ChangeBtnDisapper(Image btn, float duration)
+    {
+        float elapsed = 0.0f;
+
+        Color btnStartColor = btn.color;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration;
+            float tcolorA = Mathf.SmoothStep(btnStartColor.a, 0, t);
+            btn.color = new(btnStartColor.r, btnStartColor.g, btnStartColor.b, tcolorA);
+            yield return null;
+        }
 
     }
 }
