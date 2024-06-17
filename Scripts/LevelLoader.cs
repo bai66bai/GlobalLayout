b6d1754d90d821c02a7519c0a982263799719289
FAll <940313262@qq.com>
2024-06-17 11:34:26 +0800

    feat: 初始化客户端通信

--- Scripts/LevelLoader.cs
+++ Scripts/LevelLoader.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,18 +10,33 @@ public class LevelLoader : MonoBehaviour
 
     public float transitionTime = 1f;
 
+    private TCPClient client;
+
+    private void Start()
+    {
+        client = GetComponent<TCPClient>();
+    }
+
+
+    public void LoadNewScene(string sceneName, bool shouldSend = true)
+    {
+        StartCoroutine(LoadLevel(sceneName, shouldSend));
+    }
     public void LoadNewScene(string sceneName)
     {
-        StartCoroutine(LoadLevel(sceneName));
+        StartCoroutine(LoadLevel(sceneName, true));
     }
-   
+
     public void GoBack()
     {
         LoadNewScene(LevelStore.LastSceneName);
     }
 
-    IEnumerator LoadLevel(string sceneName)
+    IEnumerator LoadLevel(string sceneName, bool shouldSend)
     {
+        if (shouldSend && sceneName != "MenuScene")
+            client.SendMsg($"loadScene:{sceneName}");
+
         // ²¥·Å¶¯»­
         animator.SetTrigger("StartTrigger");
 
