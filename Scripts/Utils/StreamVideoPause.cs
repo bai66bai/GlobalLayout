57f56f5e9c796e21f78abc9b700fb13697c87696
FAll <940313262@qq.com>
2024-10-30 10:30:20 +0800

    feat: 完成菜单修改10/30

---
+++ Scripts/Utils/StreamVideoPause.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Video;
+
+public class StreamVideoPause : MonoBehaviour
+{
+    public VideoPlayer VideoPlayer;
+    public string VideoName;
+
+    private void Start()
+    {
+        if (VideoPlayer != null)
+        {
+            Debug.Log(VideoName);
+            VideoFile(VideoName, null, VideoPlayer);
+        }
+    }
+
+    public void VideoFile(string videoName, VLCPlayerExample vLCPlayerExample, VideoPlayer videoPlayer)
+    {
+        StartCoroutine(ReadFile(videoName, vLCPlayerExample, videoPlayer));
+    }
+
+
+    IEnumerator ReadFile(string videoName, VLCPlayerExample vLCPlayerExample, VideoPlayer videoPlayer)
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, videoName);
+
+        UnityWebRequest www = UnityWebRequest.Get(filePath);
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            if (vLCPlayerExample != null)
+            {
+                vLCPlayerExample.path = filePath;
+                Debug.Log(0);
+                // 设置VideoPlayer的视频剪辑
+                vLCPlayerExample.StartVideoWithUrlAsync(filePath);
+            }
+            else if (videoPlayer != null)
+            {
+                videoPlayer.url = filePath;
+                VideoPlayer.Prepare();
+                videoPlayer.Pause();
+            }
+
+        }
+        else
+        {
+            Debug.LogError("加载失败： " + www.error);
+        }
+    }
+
+    //在切换场景时，将视频缓存清掉
+    private void OnDestroy()
+    {
+        VideoPlayer.targetTexture.Release();
+    }
+}
